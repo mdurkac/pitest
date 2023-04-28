0c7ca62a0bae1902dd988e3bfcb504d80ba1a060
Martin Durkac <mdurkac.martin998@gmail.com>
2023-04-28 16:34:23 +0200

    Updated mutator

--- pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/experimental/spring/PermitAllOnUrlRequiringAuthenticationMutator.java
+++ pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/experimental/spring/PermitAllOnUrlRequiringAuthenticationMutator.java
@@ -45,17 +45,18 @@ public enum PermitAllOnUrlRequiringAuthenticationMutator implements MethodMutato
         public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
             if (owner.equals("org/springframework/security/config/annotation/web/configurers/ExpressionUrlAuthorizationConfigurer$AuthorizedUrl")
                     && name.equals("authenticated")
-                    && opcode == Opcodes.INVOKEVIRTUAL
-                    && desc.equals("()Lorg/springframework/security/config/annotation/web/configurers/ExpressionUrlAuthorizationConfigurer$ExpressionInterceptUrlRegistry;")
-                    && !itf) {
+                    && desc.equals("()Lorg/springframework/security/config/annotation/web/configurers/ExpressionUrlAuthorizationConfigurer$ExpressionInterceptUrlRegistry;")) {
                 final MutationIdentifier newId = context.registerMutation(factory, "Replacing authenticated with permitAll");
                 if (context.shouldMutate(newId)) {
-                    mv.visitMethodInsn(
-                            Opcodes.INVOKEVIRTUAL,
-                            "org/springframework/security/config/annotation/web/configurers/ExpressionUrlAuthorizationConfigurer$AuthorizedUrl",
-                            "permitAll",
-                            "()Lorg/springframework/security/config/annotation/web/configurers/ExpressionUrlAuthorizationConfigurer$ExpressionInterceptUrlRegistry;",
-                            false);
+                    mv.visitMethodInsn(opcode, owner, "permitAll", desc, itf);
+                    return;
+                }
+            } else if (owner.equals("org/springframework/security/config/annotation/web/configurers/AuthorizeHttpRequestsConfigurer$AuthorizedUrl")
+                    && name.equals("authenticated")
+                    && desc.equals("()Lorg/springframework/security/config/annotation/web/configurers/AuthorizeHttpRequestsConfigurer$AuthorizationManagerRequestMatcherRegistry;")) {
+                final MutationIdentifier newId = context.registerMutation(factory, "Replacing authenticated with permitAll");
+                if (context.shouldMutate(newId)) {
+                    mv.visitMethodInsn(opcode, owner, "permitAll", desc, itf);
                     return;
                 }
             }
